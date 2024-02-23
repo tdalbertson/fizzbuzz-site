0f6b45e85a5a459ea38e29fa526f6ed67445c568
Tim Albertson <68976799+tdalbertson@users.noreply.github.com>
2024-02-22 17:36:37 -0800

    Complete getfizzBuzzResults()
    - Function iterates over every number provided by user input.
    - A key-value pair is added to the fizzBuzzResults object with the
    number and it's corresponding result.

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const submitBtn = document.getElementById("submit-btn");
 const resultsBox = document.getElementById("fizzbuzz-results");
 const fizzBuzzResults = {};
 
+// Get fizzbuzz-results by assigning key-value pairs of the number
+// and it's corresponding Fizzbuzz result
+function getFizzBuzzResults(num) {
+    // Loop through until fizzBuzzNum
+    for (let index = 1; index <= num; index++) {
+        if (index % 3 === 0 && index % 5 === 0) {
+            fizzBuzzResults[index] = "FizzBuzz";
+        } else if (index % 3 === 0) {
+            fizzBuzzResults[index] = "Fizz";
+        } else if (index % 5 === 0) {
+            fizzBuzzResults[index] = "Buzz";
+        } else {
+            fizzBuzzResults[index] = "N/A";
+        }
+    }
+}
+
 // Get value for fizzBuzzNum then get FizzBuzz results
 submitBtn.addEventListener("click", () => {
     fizzBuzzNum = parseInt(document.getElementById("fizzbuzz-number").value);
